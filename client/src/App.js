619dc29e585c13f410d77737ac1bfb2f067e18bc
Saurav M. Hiremath <sauravhiremath@gmail.com>
2021-04-11 22:01:26 +0530

    feat: remove landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import 'quickdraw-component/quickdraw-component';
-import DoodlePad from './DoodlePad';
-import Landing from './Landing';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "quickdraw-component/quickdraw-component";
+import DoodlePad from "./DoodlePad";
+import Landing from "./Landing";
 
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				<Switch>
-					<Route exact path="/doodle">
-						<DoodlePad />
-					</Route>
-					<Route exact path="/">
-						<Landing />
-					</Route>
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <DoodlePad />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
